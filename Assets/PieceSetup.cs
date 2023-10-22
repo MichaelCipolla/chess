86af900c07805d704fc13434ba2cdd06094ea897
mcipolla <michael.cipolla.eemc@gmail.com>
2023-10-22 12:25:32 -0700

    mike-CapturingImplementation - Implemented Queen movement. Rook and Bishop and King still remain. - Need to refactor at some point.

--- Assets/PieceSetup.cs
+++ Assets/PieceSetup.cs
@@ -1,6 +1,7 @@
 using MyBox;
 using System;
 using Unity.VisualScripting;
+using UnityEngine;
 
 namespace ChessClasses {
     class PawnPiece : GamePiece {
@@ -150,15 +151,94 @@ namespace ChessClasses {
             byte pieceData = ChessData.getPieceData(endIndex);
 
             int moveAmount = endIndex - startIndex;
-            int rank = startIndex % 8;
-            int minRank = rank;
-            int maxRank = 7 - rank;
-            if (moveAmount >= -minRank && moveAmount <= maxRank) {
+            int filePosition = startIndex % 8;
+            int minFile = filePosition;
+            int maxFile = 7 - filePosition;
+
+            int minRank = (int)Math.Floor(startIndex / 8.0f);
+            int maxRank = 7 - minRank;
+
+            // Could check if endIndex is < startIndex - filePosition, or > 8 - filePosition
+            // If true, we need to check each index between endIndex and start index, if there's
+            // a piece, we want to disqualify the move.
+            
+            // Check if the piece is in the horizontal bounds...
+            if (moveAmount >= -minFile && moveAmount <= maxFile) {
+                int index = startIndex;
+                for(int i = 0; i < Math.Abs(moveAmount); i++) {
+                    if(startIndex > endIndex) {
+                        index--;
+                    }
+                    else {
+                        index++;
+                    }
+                    // Debug.Log("QUEEN: " + index);
+                    if(ChessData.getPieceData(index) != (byte)ChessPieceData.blank) {
+                        return false;
+                    }
+                }
+                Debug.Log("HORIZONTAL TRUE");
                 return true;
             }
-
+            // if (moveAmount >= -minFile && moveAmount <= maxFile) {
+            //     return true;
+            // }
+            Debug.Log("Validating: Move amount: " + moveAmount);
             foreach (int move in validMoveSet) {
-                if(Math.Abs(moveAmount) % move == 0) {
+                Debug.Log("Validating: move: " + move);
+                if (Math.Abs(moveAmount) % move == 0) {
+                    // Find all multiples, negative or positive:
+                    // Check startIndex + (move * i)
+                    int validMove = move;
+                    validMove *= moveAmount < 0 ? -1 : 1; 
+                    int boundary = 0;
+                    if (validMove == 7 || validMove == -9) {
+                        boundary = minFile;
+                        Debug.Log("Validating minFile: " + boundary);
+                    }
+                    else if(validMove == 9 || validMove == -7) {
+                        boundary = maxFile;
+                        Debug.Log("Validating maxFile: " + boundary);
+                    }
+                    else { // In this condition we handle the +-8 movement
+                        if(endIndex > startIndex) {
+                            boundary = maxRank; // anything above 60 is invalid. Anything below 0 is invalid.
+                        }
+                        else {
+                            boundary = minRank;
+                        }
+                    }
+                    
+                    
+                    if (endIndex > startIndex) {
+                        for (int i = 1; i <= boundary; i++) {
+                            int nextIndex = startIndex + move * i;
+                            Debug.Log("QUEEN great: " + nextIndex);
+                            if (nextIndex > endIndex) {
+                                break;
+                            }
+                            
+                            if (ChessData.getPieceData(nextIndex) != (byte)ChessPieceData.blank) {
+                                return false;
+                            }
+                        }
+                    }
+                    else {
+                        for (int i = 1; i <= boundary; i++) {
+                            int nextIndex = startIndex + move * -i;
+                            Debug.Log("QUEEN forEach: " + nextIndex);
+                            if(nextIndex < endIndex) {
+                                Debug.Log("QUEEN break!" + nextIndex);
+                                break;
+                            }
+                            
+                            if(ChessData.getPieceData(nextIndex) != (byte)ChessPieceData.blank) {
+                                Debug.Log("QUEEN occupied!" + nextIndex);
+                                return false;
+                            }
+                            Debug.Log("QUEEN fin!" + nextIndex);
+                        }
+                    }
                     return true;
                 } 
             }
