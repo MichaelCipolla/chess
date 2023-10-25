3fcc0e2b01f3a1b189f7e1f44783072628c24643
mcipolla <michael.cipolla.eemc@gmail.com>
2023-10-24 19:19:52 -0700

    mike-CapturingImplementation - Implemented the Rook and Bishop.

--- Assets/PieceSetup.cs
+++ Assets/PieceSetup.cs
@@ -97,36 +97,187 @@ namespace ChessClasses {
     }
 
     class BishopPiece : GamePiece {
-        public BishopPiece(PieceColor color) : base(color) {
+        private int[] validMoveSet = null;
 
+        public BishopPiece(PieceColor color) : base(color) {
+            this.validMoveSet = new[] { 7, 9 };
         }
+
+        public override bool capturePiece(int startIndex, int endIndex) {
+            GamePiece pieceToDestroy = ChessData.getGamePiece(endIndex);
+            if (this.pieceColor != pieceToDestroy.pieceColor) {
+                pieceToDestroy.gameObject.SetActive(false);
+                ChessData.setGamePiece(endIndex, null);
+                UnityEngine.Object.Destroy(pieceToDestroy.gameObject);
+                return true;
+            }
+            return false;
+        }
+
         public override bool validateMove(int startIndex, int endIndex) {
             byte pieceData = ChessData.getPieceData(endIndex);
 
+            int moveAmount = endIndex - startIndex;
+            int filePosition = startIndex % 8;
+            int minFile = filePosition;
+            int maxFile = 7 - filePosition;
+
+            int minRank = (int)Math.Floor(startIndex / 8.0f);
+            int maxRank = 7 - minRank;
+
+
+            foreach (int move in validMoveSet) {
+                if (Math.Abs(moveAmount) % move == 0) {
+                    // Find all multiples, negative or positive:
+                    // Check startIndex + (move * i)
+                    int validMove = move;
+                    validMove *= moveAmount < 0 ? -1 : 1; 
+                    int boundary = 0;
+                    if (validMove == 7 || validMove == -9) {
+                        boundary = minFile;
+                    }
+                    else if (validMove == 9 || validMove == -7) {
+                        boundary = maxFile;
+                    }
+                    else { // In this condition we handle the +-8 movement
+                        if (endIndex > startIndex) {
+                            boundary = maxRank; // anything above 63 is invalid. Anything below 0 is invalid.
+                        }
+                        else {
+                            boundary = minRank;
+                        }
+                    }
+                    return this.diagonalMove(startIndex, endIndex, moveAmount, move, boundary, pieceData);
+                }
+            }
+            return false;
+        }
+
+        public bool diagonalMove(int startIndex, int endIndex, int moveAmount, int move, int boundary, byte pieceData) {
+            for (int i = 1; i <= boundary; i++) {
+                int moveDirection = endIndex > startIndex ? 1 : -1;
+                int nextIndex = startIndex + move * i * moveDirection;
+                
+                if (nextIndex == endIndex) {
+                    break;
+                }
+
+                if (ChessData.getPieceData(nextIndex) != (byte)ChessPieceData.blank) {
+                    return false;
+                }
+            }
+            
             if (pieceData != (byte)ChessPieceData.blank) {
                 // We should execute capture logic here...
                 // Based on capture logic, we can further validate the move...
-                return false;
+                return this.capturePiece(startIndex, endIndex);
             }
-            // Here we will check if the requested move is valid.
             return true;
         }
     }
 
     class RookPiece : GamePiece {
+        private int[] validMoveSet = null;
+
         public RookPiece(PieceColor color) : base(color) {
-
+            this.validMoveSet = new[] { 8 };
         }
-        public override bool validateMove(int startIndex, int endIndex) {
-            byte pieceData = ChessData.getPieceData(endIndex);
 
+        public override bool capturePiece(int startIndex, int endIndex) {
+            GamePiece pieceToDestroy = ChessData.getGamePiece(endIndex);
+            if (this.pieceColor != pieceToDestroy.pieceColor) {
+                pieceToDestroy.gameObject.SetActive(false);
+                ChessData.setGamePiece(endIndex, null);
+                UnityEngine.Object.Destroy(pieceToDestroy.gameObject);
+                return true;
+            }
+            return false;
+        }
+
+        public bool diagonalMove(int startIndex, int endIndex, int moveAmount, int move, int boundary, byte pieceData) {
+            for (int i = 1; i <= boundary; i++) {
+                int moveDirection = endIndex > startIndex ? 1 : -1;
+                int nextIndex = startIndex + move * i * moveDirection;
+                
+                if (nextIndex == endIndex) {
+                    break;
+                }
+
+                if (ChessData.getPieceData(nextIndex) != (byte)ChessPieceData.blank) {
+                    return false;
+                }
+            }
+            
             if (pieceData != (byte)ChessPieceData.blank) {
                 // We should execute capture logic here...
                 // Based on capture logic, we can further validate the move...
-                return false;
+                return this.capturePiece(startIndex, endIndex);
             }
-            // Here we will check if the requested move is valid.
             return true;
+        }
+
+        public bool horizontalMove(int startIndex, int endIndex, int moveAmount, byte pieceData) {
+            int index = startIndex;
+            for(int i = 0; i < Math.Abs(moveAmount); i++) {
+                index += startIndex > endIndex ? -1 : 1; 
+
+                if(index == endIndex) {
+                    break;
+                }
+                
+                if(ChessData.getPieceData(index) != (byte)ChessPieceData.blank) {
+                    return false;
+                }
+            }
+            if (pieceData != (byte)ChessPieceData.blank) {
+                // We should execute capture logic here...
+                // Based on capture logic, we can further validate the move...
+                return this.capturePiece(startIndex, endIndex);
+            }
+            return true;
+        }
+
+        public override bool validateMove(int startIndex, int endIndex) {
+            byte pieceData = ChessData.getPieceData(endIndex);
+
+            int moveAmount = endIndex - startIndex;
+            int filePosition = startIndex % 8;
+            int minFile = filePosition;
+            int maxFile = 7 - filePosition;
+
+            int minRank = (int)Math.Floor(startIndex / 8.0f);
+            int maxRank = 7 - minRank;
+
+            // Check if the piece is in the horizontal bounds...
+            if (moveAmount >= -minFile && moveAmount <= maxFile) {
+                return this.horizontalMove(startIndex, endIndex, moveAmount, pieceData);
+            }
+
+            foreach (int move in validMoveSet) {
+                if (Math.Abs(moveAmount) % move == 0) {
+                    // Find all multiples, negative or positive:
+                    // Check startIndex + (move * i)
+                    int validMove = move;
+                    validMove *= moveAmount < 0 ? -1 : 1; 
+                    int boundary = 0;
+                    if (validMove == 7 || validMove == -9) {
+                        boundary = minFile;
+                    }
+                    else if (validMove == 9 || validMove == -7) {
+                        boundary = maxFile;
+                    }
+                    else { // In this condition we handle the +-8 movement
+                        if (endIndex > startIndex) {
+                            boundary = maxRank; // anything above 63 is invalid. Anything below 0 is invalid.
+                        }
+                        else {
+                            boundary = minRank;
+                        }
+                    }
+                    return this.diagonalMove(startIndex, endIndex, moveAmount, move, boundary, pieceData);
+                }
+            }
+            return false;
         }
     }
 
@@ -136,6 +287,7 @@ namespace ChessClasses {
         public QueenPiece(PieceColor color) : base(color) {
             this.validMoveSet = new[] { 7, 8, 9 }; 
         }
+
         public override bool capturePiece(int startIndex, int endIndex) {
             GamePiece pieceToDestroy = ChessData.getGamePiece(endIndex);
             if (this.pieceColor != pieceToDestroy.pieceColor) {
